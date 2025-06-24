362a7f59f9ad5859251ae3f6a56f2f80a6ba0bcb
estor8374 <estore8374@gmail.com>
2025-06-24 20:59:56 +0530

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+// firebase-config.js
+export const firebaseConfig = {
+  apiKey: "AIzaSyChO8KXCdEk1OzF8s95jH3g9js7J3h6LC4",
+  authDomain: "location-6a7f5.firebaseapp.com",
+  databaseURL: "https://location-6a7f5-default-rtdb.firebaseio.com",
+  projectId: "location-6a7f5",
+  storageBucket: "location-6a7f5.firebasestorage.app",
+  messagingSenderId: "421846168553",
+  appId: "1:421846168553:web:c205288f5910b5c1fe5178",
+  measurementId: "G-XLN2LQPXQY"
+};
